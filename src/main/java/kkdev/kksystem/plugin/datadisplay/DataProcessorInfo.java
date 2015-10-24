f3392d04b01bcb43838b558ca3d066809a66d5ab
Garikk <supergarikk@gmail.com>
2015-10-24 19:40:18 +0300

    Optimize

--- src/main/java/kkdev/kksystem/plugin/datadisplay/DataProcessorInfo.java
+++ src/main/java/kkdev/kksystem/plugin/datadisplay/DataProcessorInfo.java
@@ -5,7 +5,7 @@
  */
 package kkdev.kksystem.plugin.datadisplay;
 
-import kkdev.kksystem.base.constants.PluginConsts.KK_PLUGIN_TYPE;
+
 import kkdev.kksystem.base.classes.plugins.PluginInfo;
 import kkdev.kksystem.base.classes.plugins.simple.IPluginInfoRequest;
 import kkdev.kksystem.base.constants.PluginConsts;
@@ -22,7 +22,6 @@ public class DataProcessorInfo implements IPluginInfoRequest  {
         Ret.PluginUUID="b5b50412-c02a-4674-a112-ddc5805ea4e5";
         Ret.PluginName="KKDataDisplay";
         Ret.PluginDescription="Data processor for display";
-        Ret.PluginType = KK_PLUGIN_TYPE.PLUGIN_PROCESSOR;
         Ret.PluginVersion=1;
         Ret.Enabled=true;
         Ret.ReceivePins = GetMyReceivePinInfo();
